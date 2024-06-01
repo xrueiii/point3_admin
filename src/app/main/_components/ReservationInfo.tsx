c3e774d6f1fea10eaf66201f28f790b1f9be8a60
xrueiii <b11705043@g.ntu.edu.tw>
2024-06-02 02:37:05 +0800

    beautiful

--- src/app/main/_components/ReservationInfo.tsx
+++ src/app/main/_components/ReservationInfo.tsx
@@ -1,8 +1,7 @@
 "use client";
 
-import ClearIcon from "@mui/icons-material/Clear";
-import ReservationDialog from "./ReservationDialog";
 import { useState } from "react";
+import ReservationDialog from "./ReservationDialog";
 type ReservationProps = {
   name: string;
   phone: string;
@@ -10,29 +9,43 @@ type ReservationProps = {
   date: string;
   room: string;
   span: string;
-}
+};
 
-export default function ReservationInfo({ name, phone, email, date, room, span}: ReservationProps) {
+export default function ReservationInfo({
+  name,
+  phone,
+  email,
+  date,
+  room,
+  span,
+}: ReservationProps) {
   const [dialogOpen, setDialogOpen] = useState(false);
 
   return (
-      <div className="w-full h-[25%]">
-        <button
-          onClick={() => setDialogOpen(true)}
-          className="lg:text-lg text-sm h-full items-center rounded-md w-full text-black text-left p-2 bg-[#FFE900] border-[#FFE900] border-2"
-        >
-          <p>姓名：</p>
-          <p>{name}</p>
-          <p>電話：</p>
-          <p>{phone}</p>
-          <p>信箱：</p>
-          <p className="break-all">{email}</p>
-        </button>
-        <ReservationDialog onClose={() => setDialogOpen(false)} open={dialogOpen} date={date} room={room} time={`${span+10}:00~${span+11}:00`} name={name} phone={phone} email={email}/>
-        
-        {/* <button className="absolute top-0 right-0 -mt-2 -mr-2 bg-red-300 rounded-full p-1 hover:bg-red-500 w-6 h-6 flex items-center justify-center">
+    <div className="w-full h-[25%]">
+      <button
+        onClick={() => setDialogOpen(true)}
+        className="lg:text-base text-sm h-full items-center rounded-md w-full text-black text-left p-2 bg-[#FFE900] border-[#FFE900] border-2"
+      >
+        <p>姓名：</p>
+        <p>{name}</p>
+        <p>電話：</p>
+        <p>{phone}</p>
+      </button>
+      <ReservationDialog
+        onClose={() => setDialogOpen(false)}
+        open={dialogOpen}
+        date={date}
+        room={room}
+        time={`${span + 10}:00~${span + 11}:00`}
+        name={name}
+        phone={phone}
+        email={email}
+      />
+
+      {/* <button className="absolute top-0 right-0 -mt-2 -mr-2 bg-red-300 rounded-full p-1 hover:bg-red-500 w-6 h-6 flex items-center justify-center">
           <ClearIcon />
         </button> */}
-      </div>
+    </div>
   );
 }
